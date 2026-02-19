9c44eaf560010c7b1f1ecf25d9db2e2d02c377b3
Pallavi Rawal <pallavirawal679@gmail.com>
2026-02-20 00:50:28 +0530

    Clean up admin_logout.php and streamline redirection
    Removed comments and adjusted header redirection.

--- admin/admin_logout.php
+++ admin/admin_logout.php
@@ -1,11 +1,9 @@
 <?php 
 session_start();
 
-// Clear admin session variables
 unset($_SESSION['admin_id']);
 unset($_SESSION['admin_username']);
 
-// Redirect to login page
-header("Location: ../auth/login.php"); // relative to admin folder
+header("Location: ../auth/login.php"); 
 exit;
 ?>
